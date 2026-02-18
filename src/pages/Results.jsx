3b0af4dd11c80bbf96b19b2ccbad86f8069f54da
Rahul Pal <rahulpal14093@gmail.com>
2026-02-18 14:03:12 +0530

    Create Results page with analytics

---
+++ src/pages/Results.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Results = () => {
+    return (
+        <div>
+            <h1>Results Page</h1>
+            <p>This page displays the results of the quiz along with analytics.</p>
+        </div>
+    );
+};
+
+export default Results;
