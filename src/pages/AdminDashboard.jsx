060dce051b7eb58ffd9f9697d40abac3926a4293
Rahul Pal <rahulpal14093@gmail.com>
2026-02-18 14:02:49 +0530

    Create AdminDashboard component

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <p>Welcome to the Admin Panel</p>
+        </div>
+    );
+};
+
+export default AdminDashboard;
