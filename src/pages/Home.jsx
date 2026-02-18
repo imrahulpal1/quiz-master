7dbf3d4460d7024b5efb4522b174f7e5bd2295a8
Rahul Pal <rahulpal14093@gmail.com>
2026-02-18 14:02:33 +0530

    Create Home component for landing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Quiz App!</h1>
+            <p>Your journey to knowledge starts here.</p>
+        </div>
+    );
+};
+
+export default Home;
