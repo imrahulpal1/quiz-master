ea2849994995a314631795de1809ab9bf05fcb32
Rahul Pal <rahulpal14093@gmail.com>
2026-02-18 14:03:01 +0530

    Create UserQuiz component

---
+++ src/pages/UserQuiz.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserQuiz = () => {
+    return (
+        <div>
+            <h1>User Quiz Interface</h1>
+            {/* Add your quiz interface here */}
+        </div>
+    );
+};
+
+export default UserQuiz;
